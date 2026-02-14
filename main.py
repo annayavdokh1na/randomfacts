481f8ddb1d3897688c821ccbd1aea5f98cb5e92f
anna <anna.o.yavdokina@gmail.com>
2026-02-14 14:38:13 +0200

    add endpoind hello

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ app.include_router(fact_en)
 app.include_router(fact_ua)
 if __name__ == "__main__":
     uvicorn.run("main:app",reload=True)
+
+@app.get('/')
+def hello():
+    return ('EN: Hello! To access Swagger, you need to add /docs to the current URL.',
+            'UA: Привіт! Щоб перейти в Swagger, вам треба додати /docs до вашого посилання ')
